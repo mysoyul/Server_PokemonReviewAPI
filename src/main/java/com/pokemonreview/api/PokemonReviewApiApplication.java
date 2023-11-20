472ce359a80f398a5e9aa15d2bb3f60a76cb615b
mysoyul <vega2k_@naver.com>
2023-11-20 17:30:48 +0900

    review service update

--- src/main/java/com/pokemonreview/api/PokemonReviewApiApplication.java
+++ src/main/java/com/pokemonreview/api/PokemonReviewApiApplication.java
@@ -1,7 +1,16 @@
 package com.pokemonreview.api;
 
+import com.pokemonreview.api.models.Pokemon;
+import com.pokemonreview.api.models.PokemonType;
+import com.pokemonreview.api.repository.PokemonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class PokemonReviewApiApplication {
@@ -10,4 +19,21 @@ public class PokemonReviewApiApplication {
 		SpringApplication.run(PokemonReviewApiApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner test(PokemonRepository pokemonRepository) {
+		return args -> {
+			System.out.println("**** Pokemon Insert 시작");
+			pokemonRepository.deleteAll();
+			List<Pokemon> pokemonList = IntStream.range(0, 10)
+					.mapToObj(i -> Pokemon.builder()
+							.name("pikachu" + i)
+							.type(PokemonType.ELECTRIC)
+							.build())
+					.collect(Collectors.toList());
+
+			pokemonRepository.saveAll(pokemonList);
+			System.out.println("**** Pokemon Insert 끝");
+
+		};
+	}
 }
